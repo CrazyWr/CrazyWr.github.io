85521720edb1c3c49bfb31d9c730553a8a3f3f82
wangqingfeng <990335585@qq.com>
2020-10-21 14:35:14 +0800

    substitute image

--- images/substitute_image.sh
+++ images/substitute_image.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-cp /Users/wangqingfeng/CProject/CrazyWr.github.io/images1/$1 /Users/wangqingfeng/CProject/CrazyWr.github.io/images1/xiaohaizi.jpeg
+cp /Users/wangqingfeng/CProject/CrazyWr.github.io/images/$1 /Users/wangqingfeng/CProject/CrazyWr.github.io/images1/xiaohaizi.jpeg
 git add .
 git commit -m 'substitute image'
 git push
